67b365e23bab5feed4d0f81c8445d96a47c879af
sophiexsf <95551648+sophiexsf@users.noreply.github.com>
2022-03-03 21:18:06 -0500

    deliverable 2

--- scraping.py
+++ scraping.py
@@ -76,6 +76,42 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     return df.to_html()
 
+def hemisphere_images(browser):
+    # ### Hemispheres
+
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    hemisphere_soup = soup(html, 'html.parser')
+    box = hemisphere_soup.find('div', class_='collapsible results')
+    names = box.find_all('h3')
+    for name in names:
+        hemispheres = {}
+        name = name.text
+        browser.links.find_by_partial_text(name).click()
+        html = browser.html
+        html_soup = soup(html, 'html.parser')
+        img_url_rel = html_soup.find('a', text="Sample").get("href")
+        img_url = f'https://marshemispheres.com/{img_url_rel}'
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = html_soup.find('h2', class_="title").text
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -88,7 +124,8 @@ def scrape_all():
       "news_paragraph": news_paragraph,
       "featured_image": featured_image(browser),
       "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
+      "last_modified": dt.datetime.now(),
+      "hemispheres": hemisphere_images(browser)
     }
 
     # Stop webdriver and return data
